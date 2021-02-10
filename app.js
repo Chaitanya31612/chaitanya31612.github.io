d87be07225f4d1ffa1b0c5e6a6f2d70bf76c1a8c
Chaitanya31612 <guptachaitanya31@gmail.com>
2021-02-10 22:49:36 +0530

    hamburger and darkmode

--- app.js
+++ app.js
@@ -8,3 +8,51 @@ function scrollFunction() {
     document.querySelector(".nav").style.top = "-70px";
   }
 }
+
+let loader = document.querySelector('.loader');
+
+window.addEventListener("load", vanish);
+function vanish() {
+    loader.classList.add("disappear")
+}
+
+
+// function for toggling hamburger is-active class
+$(function(){
+  
+  $("#js-hamburger").on("click", function(){
+    $(this).toggleClass('is-active');
+    $(".nav__links").toggleClass('visible');
+    $(".nav__title").toggleClass('not-active');
+  });
+
+});
+
+//dark mode toggle
+$(function(){
+  
+  $(".toggle--dark").on("click", function(){
+    document.documentElement.classList.add('dark-mode')
+    
+    document.querySelectorAll('.t-btn').forEach((result) => {
+      result.classList.add('same')
+    })
+
+    document.querySelectorAll('.inverted').forEach((result) => {
+      result.classList.toggle('invert')
+    })
+  });
+
+});
+
+$(function(){
+  
+  $(".toggle--light").on("click", function(){
+    document.documentElement.classList.remove('dark-mode')
+    document.querySelectorAll('.t-btn').forEach((result) => {
+      result.classList.remove('same')
+    })
+  });
+
+});
+
