1cb607590082bfc586fa5111eb6100ea3f028a5d
Chaitanya <guptachaitanya31@gmail.com>
2021-11-27 00:16:43 +0530

    dark mode stop

--- app.js
+++ app.js
@@ -1,92 +1,94 @@
-
-window.onscroll = function() {scrollFunction()};
+window.onscroll = function () {
+  scrollFunction();
+};
 
 function scrollFunction() {
-  if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
+  if (
+    document.body.scrollTop > 500 ||
+    document.documentElement.scrollTop > 500
+  ) {
     document.querySelector(".nav").style.top = "0";
   } else {
     document.querySelector(".nav").style.top = "-70px";
   }
 }
 
-let loader = document.querySelector('.loader');
+let loader = document.querySelector(".loader");
 
 window.addEventListener("load", vanish);
 function vanish() {
-    loader.classList.add("disappear")
+  loader.classList.add("disappear");
 }
 
-
 // function for toggling hamburger is-active class
-$(function(){
-  
-  $("#js-hamburger").on("click", function(){
-    $(this).toggleClass('is-active');
-    $(".nav__links").toggleClass('visible');
-    $(".nav__title").toggleClass('not-active');
+$(function () {
+  $("#js-hamburger").on("click", function () {
+    $(this).toggleClass("is-active");
+    $(".nav__links").toggleClass("visible");
+    $(".nav__title").toggleClass("not-active");
   });
-
 });
 
 if (!localStorage.getItem("theme")) {
-  localStorage.setItem("theme", "light");  
+  localStorage.setItem("theme", "light");
 }
-
 
 const checkTheme = () => {
   if (localStorage.getItem("theme") == "dark") {
     DarkReader.enable({
       brightness: 100,
       contrast: 90,
-      sepia: 10
+      sepia: 10,
     });
 
-    document.getElementById("white").setAttribute('style', 'background:white !important');
+    document
+      .getElementById("white")
+      .setAttribute("style", "background:white !important");
   } else if (localStorage.getItem("theme") == "light") {
-    document.getElementById("black").setAttribute('style', 'background:black !important');
+    document
+      .getElementById("black")
+      .setAttribute("style", "background:black !important");
     DarkReader.disable();
   }
-}
+};
 
 // window.addEventListener("load", checkTheme)
 
-$(function(){
+// $(function () {
+//   $(".toggle--dark").on("click", function () {
+//     DarkReader.enable({
+//       brightness: 100,
+//       contrast: 90,
+//       sepia: 10,
+//     });
 
-  $(".toggle--dark").on("click", function () {
-    DarkReader.enable({
-        brightness: 100,
-        contrast: 90,
-        sepia: 10
-    });
+//     document
+//       .getElementById("white")
+//       .setAttribute("style", "background:white !important");
+//     // const icons = document.getElementsByClassName("social-icon")
+//     // console.log(icons);
+//     // for (let i = 0; i < icons.length; i++) {
+//     //   console.log("here");
+//     //   console.log(icons[i]);
+//     // }
 
-    document.getElementById("white").setAttribute('style', 'background:white !important');
-    // const icons = document.getElementsByClassName("social-icon")
-    // console.log(icons);
-    // for (let i = 0; i < icons.length; i++) {
-    //   console.log("here");
-    //   console.log(icons[i]);
-    // }
+//     localStorage.setItem("theme", "dark");
+//   });
+// });
 
-    localStorage.setItem("theme", "dark");
-  });
-
-});
-
-$(function(){
-  
-  $(".toggle--light").on("click", function(){
+$(function () {
+  $(".toggle--light").on("click", function () {
     DarkReader.disable();
     localStorage.setItem("theme", "light");
   });
-
 });
 
 //dark mode toggle
 // $(function(){
-  
+
 //   $(".toggle--dark").on("click", function(){
 //     document.documentElement.classList.add('dark-mode')
-    
+
 //     document.querySelectorAll('.t-btn').forEach((result) => {
 //       result.classList.add('same')
 //     })
@@ -99,7 +101,7 @@ $(function(){
 // });
 
 // $(function(){
-  
+
 //   $(".toggle--light").on("click", function(){
 //     document.documentElement.classList.remove('dark-mode')
 //     document.querySelectorAll('.t-btn').forEach((result) => {
@@ -108,4 +110,3 @@ $(function(){
 //   });
 
 // });
-
